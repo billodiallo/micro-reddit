9cbcd1495ee0a9cbba92bd28c5b573f084541249
Billo Diallo <billodiallonet@gmail.com>
2021-03-09 20:16:37 +0300

    fix linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class Post < ApplicationRecord
-  validates :title, presence:true
+  validates :title, presence: true
   belongs_to :user
   has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-    validates :name, presence: true
-  
-    has_many :posts
-    has_many :comments
-  end
+  validates :name, presence: true
+
+  has_many :posts
+  has_many :comments
+end
